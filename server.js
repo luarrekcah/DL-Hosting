f8ba7e62cd2c6139e14c1daa26824753a63810ea
luarrekcah <raulrodriguesdemoraes@gmail.com>
2023-05-19 16:59:54 -0500

    [added] server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+require('./bot');
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor ON`);
+});
